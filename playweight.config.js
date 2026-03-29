2ec732fcc0c916534af617da5c77b70711fb2be8
CalumSt <calum.stuart@protonmail.com>
2026-03-29 15:04:51 +0100

    playwright.config.js: fix CI

--- playweight.config.js
+++ playweight.config.js
@@ -2,26 +2,13 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './playwright',
-  timeout: 30000,
-  fullyParallel: true, // Run Chrome and Safari at the same time
+  fullyParallel: true,
   use: {
-    baseURL: 'http://localhost:5000',
-    trace: 'on-first-retry',
-  },
-  // This starts your server automatically so you don't have to & it in CI
-  webServer: {
-    command: 'npx serve . -l 5000',
-    url: 'http://localhost:5000',
-    reuseExistingServer: !process.env.CI,
+    // This will be overridden by the ENV var in the workflow, but good for local
+    baseURL: 'http://127.0.0.1:5000', 
   },
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
   ],
 });
