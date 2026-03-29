5addca16694b7f663cc373a7b430f6d9aea8e8aa
CalumSt <calum.stuart@protonmail.com>
2026-03-29 14:58:37 +0100

    playwright.config.js: add chrome tests

--- playweight.config.js
+++ playweight.config.js
@@ -1,13 +1,27 @@
-// playwright.config.js
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './playwright',
   timeout: 30000,
+  fullyParallel: true, // Run Chrome and Safari at the same time
   use: {
     baseURL: 'http://localhost:5000',
+    trace: 'on-first-retry',
+  },
+  // This starts your server automatically so you don't have to & it in CI
+  webServer: {
+    command: 'npx serve . -l 5000',
+    url: 'http://localhost:5000',
+    reuseExistingServer: !process.env.CI,
   },
   projects: [
-    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 });
